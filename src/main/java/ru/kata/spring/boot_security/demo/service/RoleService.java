b35b9ac631c6b8be8a2876d664ce608168ec9394
Evgeniy Filipenko <berumoris@gmail.com>
2022-03-08 03:58:37 +0300

    done

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.service;
-
-import ru.kata.spring.boot_security.demo.model.Role;
-
-public interface RoleService {
-    void createRole(Role nameRole);
-    Role findByNameRole(String nameRole);
-}
-
